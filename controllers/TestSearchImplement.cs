5d20967a373f874a07477ad0503dbdcd974f37c8
khalil <khalillakhdharatc@gmail.com>
2022-02-01 15:47:05 +0100

    correction

---
+++ controllers/TestSearchImplement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CovidTuto.interfaces;
+namespace CovidTuto.controllers
+{
+    class TestSearchImplement : TestSearchInterface
+    {
+        public void searchRapportPositif(List<TestModel> testes)
+        {
+            Console.WriteLine("searchRapportPositif ");
+        }
+
+        public void searchReportPatientById(int idPatient, List<TestModel> testes)
+        {
+            Console.WriteLine("searchReportPatientById");
+        }
+    }
+}

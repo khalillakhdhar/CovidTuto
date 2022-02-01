dced718d1ac317df0e916fae2b247b299bd92340
khalil <khalillakhdharatc@gmail.com>
2022-02-01 14:47:01 +0100

    update

---
+++ controllers/TestImplement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CovidTuto.interfaces;
+namespace CovidTuto.controllers
+{
+    class TestImplement : TestInterface
+    {
+        public void afficher(List<TestModel> t)
+        {
+            foreach (var tests in t)
+            {
+               Console.WriteLine(t.ToString());
+
+            }
+        }
+
+        public void ajouter(TestModel t)
+        {
+            Console.WriteLine(t.ToString() + " ajouté ");
+        }
+    }
+}
---
+++ interfaces/TestInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidTuto.interfaces
+{
+    interface TestInterface
+    {
+        void ajouter(TestModel t);
+        void afficher(List<TestModel> t);
+    }
+}
